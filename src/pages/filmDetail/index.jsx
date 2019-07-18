08bfa396b761c2da394ac57cae11e8995643f47c
eduardo <eduregis1@gmail.com>
2019-07-18 14:15:54 -0300

    redux pegando e enviando dados do store

--- src/pages/filmDetail/index.jsx
+++ src/pages/filmDetail/index.jsx
@@ -1,16 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-const FilmDetail = ({ modules }) => (
+const FilmDetail = ({ modules, teste, dispatch }) => (
     <div>
-        {modules.map(module => (
-            <div key={module.id}>
-                <div>{module.title}</div>
-            </div>
-        ))}        
+        <h2>{teste}</h2>
+        <button onClick={() => dispatch(toggleTest('test3'))}>trocar</button>
+        <div>
+            {modules.map(module => (
+                <div key={module.id}>
+                    <div>{module.title}</div>
+                </div>
+            ))}        
+        </div>
     </div>
+   
 );
 
+function toggleTest(str){
+    return {
+        type: 'SET_TEST',
+        str,
+    }
+}
 
 
-export default connect(state => ({ modules: state }))(FilmDetail);
+
+export default connect(
+    state => ({ 
+        teste: state.teste,
+        modules: state.modules 
+    }))(FilmDetail);
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import { createStore } from 'redux';
 
-function reducer() {
-    return [
+const INITIAL_STATE = {
+    teste: 'test2',
+    modules:[
         {
             id: 1,
-            title: "teste"
+            title: "test"
         }
-    ];
+    ]
+}
+
+function reducer(state = INITIAL_STATE, action) {
+    console.log(action)
+    if(action.type === 'SET_TEST'){
+        return { ...state, teste: action.str }
+    }
+    return state;
 }
 
 const store = createStore(reducer);
